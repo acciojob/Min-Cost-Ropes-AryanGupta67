c325d75f0c8c1d87933ea397dcaccf85398fd463
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-10 22:26:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function mincost(arr) {
+  if (arr.length <= 1) return { message: 0 };
 
-module.exports=mincost;
+  let heap = [...arr];
+  heap.sort((a, b) => a - b);
+
+  let totalCost = 0;
+
+  while (heap.length > 1) {
+    let first = heap.shift();
+    let second = heap.shift();
+
+    let cost = first + second;
+    totalCost += cost;
+
+    heap.push(cost);
+    heap.sort((a, b) => a - b);
+  }
+
+  return { message: totalCost };
+}
